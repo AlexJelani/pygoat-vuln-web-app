e0a65e1dd5d01502326c22b026496851700fa863
AlexJelani <jelanialexander82@gmail.com>
2025-08-18 15:15:02 +0900

    Reverting the Jenkinsfile.groovy to the specified commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,7 +37,8 @@ pipeline {
             docker run --rm \
               -v "${WORKSPACE}:/workspace" \
               -w /workspace \
-              ghcr.io/gitleaks/gitleaks:latest detect --source=/workspace \
+              ghcr.io/gitleaks/gitleaks:latest dir \
+              --source=/workspace \
               --verbose \
               --report-path=/workspace/reports/gitleaks-report.json \
               --report-format=json
